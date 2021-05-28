16cb309f87a995b291f2d12f0efbc2f01e5b6736
ztind <1356798719@qq.com>
2021-05-28 11:51:18 +0800

    new:add spalsh aty

---
+++ sample/src/main/java/com/zt/sample/App.kt
@@ -0,0 +1,14 @@
+package com.zt.sample
+
+import android.app.Application
+
+/**
+Describe：文件描述
+Author:ZT
+Date:2021/5/28
+ */
+class App :Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
