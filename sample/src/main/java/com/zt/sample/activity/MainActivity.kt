bc9c1d0efc9ccc622d0bda02adfe7ba25ea85cc8
ztind <1356798719@qq.com>
2021-05-28 14:51:11 +0800

    new:a sample mvvm use

--- sample/src/main/java/com/zt/sample/activity/MainActivity.kt
+++ sample/src/main/java/com/zt/sample/activity/MainActivity.kt
@@ -2,11 +2,10 @@ package com.zt.sample.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zt.mvvm.view.BaseActivity
 import com.zt.sample.R
+import com.zt.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseActivity<MainViewModel,ActivityMainBinding>() {
+    override fun setLayoutResId(): Int = R.layout.activity_main
 }
---
+++ sample/src/main/java/com/zt/sample/activity/MainRepository.kt
@@ -0,0 +1,11 @@
+package com.zt.sample.activity
+
+import com.zt.mvvm.repository.BaseRepository
+
+/**
+Describe：文件描述
+Author:ZT
+Date:2021/5/28
+ */
+class MainRepository :BaseRepository {
+}
