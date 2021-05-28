3c7e7dd27fce4719e0f06fd2139481bc1f5bd2f9
ztind <1356798719@qq.com>
2021-05-28 15:56:15 +0800

    new:add

--- sample/src/main/java/com/zt/sample/activity/MainActivity.kt
+++ sample/src/main/java/com/zt/sample/activity/MainActivity.kt
@@ -2,10 +2,23 @@ package com.zt.sample.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.zt.mvvm.view.BaseActivity
 import com.zt.sample.R
 import com.zt.sample.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<MainViewModel,ActivityMainBinding>() {
     override fun setLayoutResId(): Int = R.layout.activity_main
+    override fun initView() {
+        super.initView()
+        //取消icon的默认着色
+        mDataBinding.bottomNavView.itemIconTintList = null
+        /**
+         * 去除BottomNavigationView长按Toast通知
+         */
+        mDataBinding.bottomNavView.findViewById<BottomNavigationItemView>(R.id.navigation_home).setOnLongClickListener { true }
+        mDataBinding.bottomNavView.findViewById<BottomNavigationItemView>(R.id.navigation_dynamic).setOnLongClickListener { true }
+        mDataBinding.bottomNavView.findViewById<BottomNavigationItemView>(R.id.navigation_message).setOnLongClickListener { true }
+        mDataBinding.bottomNavView.findViewById<BottomNavigationItemView>(R.id.navigation_mine).setOnLongClickListener { true }
+    }
 }
