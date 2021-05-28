b856af5566f2f52ef9f196ad5de125c86758994f
ztind <1356798719@qq.com>
2021-05-28 10:09:28 +0800

    fix:sample modelu名字修正

--- sample/src/main/java/com/zt/sample/activity/MainActivity.kt
+++ sample/src/main/java/com/zt/sample/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zt.sample
+package com.zt.sample.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zt.sample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
